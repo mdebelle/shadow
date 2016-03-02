e85662fd0c6301a1f4b2dd3b321c2000c5e37232
Marion DEBELLEY <mdebelle@e2r5p17.42.fr>
2016-03-02 21:27:20 +0100

    add rotaion left/right, rotation up/down, move left/right/up/down

--- Assets/Moveobject.cs
+++ Assets/Moveobject.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class Moveobject : MonoBehaviour {
 
 	bool	selected;
+	bool	vertical;
+	bool	move;
+
 	float	posx;
 	float	posy;
-
 
 	// Use this for initialization
 	void Start () {
 	
 		selected = false;
+		vertical = false;
+		move = false;
+
 	}
 	
 	// Update is called once per frame
@@ -21,22 +26,72 @@ public class Moveobject : MonoBehaviour {
 		
 			Debug.Log("unselected");
 			selected = false;
+			vertical = false;
 		
 		}
 
+		if (Input.GetKeyDown (KeyCode.LeftCommand)) {
+			move = true;
+		}
+		if (Input.GetKeyUp (KeyCode.LeftCommand)) {
+			move = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftControl) && move == false) {
+			vertical = true;
+		}
+		if (Input.GetKeyUp (KeyCode.LeftControl)) {
+			vertical = false;
+		}
+
 		if (selected == true) {
-			Debug.Log ("mouse position = " + Input.mousePosition.x + " " + Input.mousePosition.y);
 
+			if (move == true){
+				
+				if (posy< Input.mousePosition.y)
+					transform.Translate(Vector3.up * Time.deltaTime * 2, Space.World);
+				else if (posy > Input.mousePosition.y)
+					transform.Translate(Vector3.down * Time.deltaTime * 2, Space.World);
 
+				if (posx < Input.mousePosition.x)
+					transform.Translate(Vector3.right * Time.deltaTime * 2, Space.World);
+				else if (posx > Input.mousePosition.x)
+					transform.Translate(Vector3.left * Time.deltaTime * 2, Space.World);
+
+			} else {
+
+				if (vertical == true && move == false) {
+
+					if (posy < Input.mousePosition.y)
+						transform.Rotate(Vector3.right * 2, Space.World);
+					else if (posy > Input.mousePosition.y)
+						transform.Rotate(Vector3.left * 2, Space.World);
+					else
+						Debug.Log("Do not move");
+			
+				} else if (vertical == false && move == false) {
+				
+					if (posx < Input.mousePosition.x)
+						transform.Rotate(Vector3.down * 2, Space.World);
+					else if (posx > Input.mousePosition.x)
+						transform.Rotate(Vector3.up * 2,Space.World);
+					else
+						Debug.Log("Do not move");
+
+				}
+
+			}
+			
+			posx = Input.mousePosition.x;
+			posy = Input.mousePosition.y;
 		}
 
 	}
 	
 	void OnMouseDown () {
 
-		Debug.Log("selected");
-		Debug.Log("screen size = " + Screen.width + " " + Screen.height);
-		Debug.Log ("mouse position = " + Input.mousePosition.x + " " + Input.mousePosition.y);
+		posx = Input.mousePosition.x;
+		posy = Input.mousePosition.y;
 		selected = true;
 
 	}
