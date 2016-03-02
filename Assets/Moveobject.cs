30eb921c8c17e12676a01dc972e7903e8a56b9b7
Marion DEBELLEY <mdebelle@e2r5p17.42.fr>
2016-03-02 19:06:08 +0100

    add some mouse position

---
+++ Assets/Moveobject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveobject : MonoBehaviour {
+
+	bool	selected;
+	float	posx;
+	float	posy;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		selected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonUp (0)) {
+		
+			Debug.Log("unselected");
+			selected = false;
+		
+		}
+
+		if (selected == true) {
+			Debug.Log ("mouse position = " + Input.mousePosition.x + " " + Input.mousePosition.y);
+
+
+		}
+
+	}
+	
+	void OnMouseDown () {
+
+		Debug.Log("selected");
+		Debug.Log("screen size = " + Screen.width + " " + Screen.height);
+		Debug.Log ("mouse position = " + Input.mousePosition.x + " " + Input.mousePosition.y);
+		selected = true;
+
+	}
+}
